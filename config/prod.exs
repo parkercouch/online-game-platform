22a4305ef28aa4fa38508654ec45ef206d17fa80
Parker Couch <parkercouch@gmail.com>
2019-02-27 20:21:27 -0800

    Update production configuration for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,16 @@ use Mix.Config
 # before starting your production server.
 config :platform, PlatformWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "online-game-platform.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forward_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :platform, Platform.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +76,5 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# USING HEROKU SO NOT USING #TODO: Change if moving from heroku
+# import_config "prod.secret.exs"
